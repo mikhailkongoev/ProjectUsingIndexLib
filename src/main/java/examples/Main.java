da85f806d898cbffcdc1958d328a1ac9d65aaf04
mikhailkongoev <mikhailkongoev@mail.ru>
2019-07-20 22:51:05 +0300

    Initial commit

---
+++ src/main/java/examples/Main.java
@@ -0,0 +1,22 @@
+package examples;
+
+import configuration.Configurations;
+import index.IndexServiceImpl;
+import interfaces.services.ConfigurationService;
+import interfaces.services.IndexService;
+import lexer.SimpleTokenizer;
+import search.SearchServiceImpl;
+
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) {
+        ConfigurationService configurationService = Configurations.createConfiguration();
+        IndexService indexService = new IndexServiceImpl(new SimpleTokenizer());
+        SearchServiceImpl abstractSearchService = new SearchServiceImpl(new SimpleTokenizer());
+
+        indexService.addToIndex((Path) null);
+
+        abstractSearchService.search("qwe ewq");
+    }
+}
