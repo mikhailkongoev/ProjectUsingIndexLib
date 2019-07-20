b665625c6d06c0f9f504a94c06586cea1d7c008e
mikhailkongoev <mikhailkongoev@mail.ru>
2019-07-21 01:44:07 +0300

    application for test

--- src/main/java/examples/Main.java
+++ src/main/java/examples/Main.java
@@ -1,22 +1,21 @@
 package examples;
 
 import configuration.Configurations;
-import index.IndexServiceImpl;
 import interfaces.services.ConfigurationService;
 import interfaces.services.IndexService;
-import lexer.SimpleTokenizer;
-import search.SearchServiceImpl;
+import interfaces.services.SearchService;
+import lexer.FilteringTokenizer;
 
 import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-        ConfigurationService configurationService = Configurations.createConfiguration();
-        IndexService indexService = new IndexServiceImpl(new SimpleTokenizer());
-        SearchServiceImpl abstractSearchService = new SearchServiceImpl(new SimpleTokenizer());
+        ConfigurationService configurationService = Configurations.createConfiguration(new FilteringTokenizer());
+        IndexService indexService = configurationService.createIndexService();
+        SearchService searchService = configurationService.createSearchService();
 
         indexService.addToIndex((Path) null);
 
-        abstractSearchService.search("qwe ewq");
+        searchService.search("qwe ewq");
     }
 }
