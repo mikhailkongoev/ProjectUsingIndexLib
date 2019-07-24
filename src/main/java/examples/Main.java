fa092242cbc2450012a68fe241fd192c09d49714
mikhailkongoev <mikhailkongoev@mail.ru>
2019-07-24 03:33:20 +0300

    Console interface added

--- src/main/java/examples/Main.java
+++ src/main/java/examples/Main.java
@@ -1,21 +1,44 @@
 package examples;
 
-import configuration.Configurations;
+import impl.configuration.Configurations;
+import impl.lexer.SimpleTokenizer;
 import interfaces.services.ConfigurationService;
 import interfaces.services.IndexService;
 import interfaces.services.SearchService;
-import lexer.FilteringTokenizer;
 
-import java.nio.file.Path;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        ConfigurationService configurationService = Configurations.createConfiguration(new FilteringTokenizer());
+    public static void main(String[] args) throws IOException {
+        ConfigurationService configurationService = Configurations.createConfiguration(new SimpleTokenizer());
         IndexService indexService = configurationService.createIndexService();
         SearchService searchService = configurationService.createSearchService();
 
-        indexService.addToIndex((Path) null);
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        searchService.search("qwe ewq");
+        while(true) {
+            System.out.println("Выберите действие: \n1 - индексировать, \n2 - удалить из индекса, \n3 - выполнить запрос к индексу, \n4 - закончить выполнение");
+            String activity = in.readLine();
+            switch (activity) {
+                case "1":
+                    System.out.println("Укажите путь, который хотите проиндексировать");
+                    indexService.addToIndex(in.readLine());
+                    break;
+                case "2":
+                    System.out.println("Укажите путь, который хотите удалить из индекса");
+                    indexService.removeFromIndex(in.readLine());
+                    break;
+                case "3":
+                    System.out.println("Введите запрос для поиска");
+                    searchService.search(in.readLine()).forEach(System.out::println);
+                    break;
+                case "4":
+                    return;
+                default:
+                    System.out.println("Вы запросили неизвестное действие! Попробуйте ещё раз.");
+            }
+        }
     }
 }
